11311536f906e570be5cef71846644701eb38082
Jamison Fox <fox.jamison@outlook.com>
2022-09-16 17:12:41 -0600

    worked on debugging

--- map-reduce-filter.js
+++ map-reduce-filter.js
@@ -1,1 +1,33 @@
+console.clear();
+
+const friends = [
+    {"Name":'Joey', "Gender":"Male"},
+    {"Name":'Ross', "Gender":"Male"},
+    {"Name":'Chandler', "Gender":"Male"},
+    {"Name":'Rachel', "Gender":"Female"},
+    {"Name":'Monica', "Gender":"Female"},
+    {"Name":'Phoebe', "Gender":"Female"}
+]
+
+const data = [
+  {"city":"seattle", "state":"WA", "population":652405, "land_area":83.9},
+  {"city":"new york", "state":"NY", "population":8405837, "land_area":302.6},
+  {"city":"boston", "state":"MA", "population":645966, "land_area":48.3},
+  {"city":"kansas city", "state":"MO", "population":467007, "land_area":315}
+];
+
+// array.map((element, index, array) => {expression})
+const mappedCityStates = data.map((lol) => {
+  return {'city': lol.city, 'state': lol.state};
+});
+
+console.log(data);
+console.log(friends);
+
+console.log("Hello World");
+
+
+
+
+
 
