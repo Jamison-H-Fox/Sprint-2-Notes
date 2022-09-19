db3cb724f68a1a87ad165747104585140a121204
Jamison Fox <fox.jamison@outlook.com>
2022-09-19 14:34:23 -0600

    Took more notes

--- map-reduce-filter.js
+++ map-reduce-filter.js
@@ -1,12 +1,12 @@
 console.clear();
 
 const friends = [
-    {"Name":'Joey', "Gender":"Male"},
-    {"Name":'Ross', "Gender":"Male"},
-    {"Name":'Chandler', "Gender":"Male"},
-    {"Name":'Rachel', "Gender":"Female"},
-    {"Name":'Monica', "Gender":"Female"},
-    {"Name":'Phoebe', "Gender":"Female"}
+    {"First Name":'Joey', "Last Name": "Tribbiani", "Gender":"Male", "Age": 24},
+    {"First Name":'Ross', "Last Name": "Gheller","Gender":"Male", "Age": 25},
+    {"First Name":'Chandler', "Last Name": "Bing","Gender":"Male", "Age": 25},
+    {"First Name":'Rachel', "Last Name": "?","Gender":"Female", "Age": 22},
+    {"First Name":'Monica', "Last Name": "Gheller","Gender":"Female", "Age": 22},
+    {"First Name":'Phoebe', "Last Name": "Buffet","Gender":"Female", "Age": 26}
 ]
 
 const data = [
@@ -24,11 +24,25 @@ const mappedCityStates = data.map((lol) => {
 console.log(data);
 console.log(friends);
 
-console.log("Hello World");
-console.log("Look ma, I'm a real programmer!");
+// syntax for .map ---> const variable = input.map((element) => {expression});
+const firstNameOnly = friends.map((key) => {
+  return {'First Name':key["First Name"]};
+});
 
+console.log(firstNameOnly);
 
+// syntax for .filter ---> const variable = input.filter((element) => {expression});
+const maleOnly = friends.filter((key) => {
+  return key["Gender"] === "Male";
+});
 
+console.log(maleOnly);
 
+// syntax for .reduce ---> const variable = input.reduce((accumulator, element) => {expresion}, start value);
+const totalAge = friends.reduce((total, element) => {
+  return total += element['Age'];
+}, 0);
+
+console.log(totalAge)
 
 
