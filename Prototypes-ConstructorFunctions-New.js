9725b14ef2b92be01000e02b34897c489319dd72
Jamison Fox <fox.jamison@outlook.com>
2022-09-22 16:37:04 -0600

    I completed the challenge for 2.3.2

--- Prototypes-ConstructorFunctions-New.js
+++ Prototypes-ConstructorFunctions-New.js
@@ -1,5 +1,6 @@
 // constructor functions are capitalized by convention. This won't change the syntax, it's just best practices
 
+/*
 function Person(attributes) {
     this.age = attributes.age;
     this.name = attributes.name;
@@ -119,7 +120,73 @@ newKiwi.checkIfFuzzy();
 newBanana.calculateCals();
 newKiwi.calculateCals();
 
+*/
 
+/* ==== Prototypes Challenge
+
+  1. Study the console.log() and object method invocations at the bottom of the page. Update the Animal and Dog constructors so that the logs and methods match the commented result next to them.
+  
+  2. Stretch Challenge: Create a new constructor function called Cat. Add properties and a method unique to a cat.  Make sure cat inherits from Animal so you can use all of the properties and methods found there.
+
+
+You can check your work here:
+
+https://codepen.io/BloomTech/pen/yxjRJa
+
+*/
+
+
+function Animal(attributes) {
+    this.weight = attributes.weight;
+    this.height = attributes.height;
+    this.food = attributes.food;
+    this.animalCommonName = attributes.animalCommonName;
+  }
+  
+  Animal.prototype.eat = function() {
+    console.log(`The ${this.animalCommonName} eats ${this.food}`);
+  }
+  
+  function Dog(dogAttributes) {
+    // Connect the attributes so we can use the this keyword
+    Animal.call(this, dogAttributes);
+    this.name = dogAttributes.name;
+    this.bark = dogAttributes.bark;
+  }
+  // Set up our __proto__ inheritance to Animal
+  Dog.prototype = Object.create(Animal.prototype);
+
+  Dog.prototype.speak = function() {
+    console.log(`${this.name} says: ${this.bark}`);
+  }
+  
+  const dog = new Dog({
+    'name': 'Dr. Doggo',
+    'animalCommonName': "dog",
+    'weight': 40,
+    'height': 12,
+    'food': 'meat',
+    'bark': 'Woof!'
+  });
+
+  const dog2 = new Dog({
+    'name': 'Lucy',
+    'animalCommonName': "dog",
+    'weight': 40,
+    'height': 12,
+    'food': 'cheese',
+    'bark': 'Bark! Bark-bark-bark-BARK!'
+  });
+  
+console.log(dog.animalCommonName); // "dog"
+dog.eat(); // "The dog eats meat"
+dog.speak(); // "Dr. Doggo says: Woof!"
+
+dog2.eat();
+dog2.speak();
+
+//   console.log(dog);
+  
 
 
 
